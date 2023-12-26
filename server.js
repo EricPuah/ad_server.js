9f2cc561e6541befbd5344d6f135dcd14ff868da
Eric Puah <ericpuah926@gmail.com>
2023-12-27 00:13:25 +0800

    qwwe

--- server.js
+++ server.js
@@ -95,8 +95,6 @@ app.post('/submit-feedback', async (req, res) => {
     }
 });
 
-let driverLocation = null;
-
 app.post('/location', async (req, res) => {
     try {
         const { lat, lng } = req.body;
